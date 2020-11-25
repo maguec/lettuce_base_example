4ee999efb85172ce3f39039d50132c0c0752549c
Chris Mague <github@mague.com>
2020-11-25 15:04:16 -0800

    async pipeline example

--- src/main/java/com/redislabs/lettucetest/App.java
+++ src/main/java/com/redislabs/lettucetest/App.java
@@ -4,38 +4,30 @@ import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisFuture;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import io.lettuce.core.support.ConnectionPoolSupport;
 
 public class App {
 
-    private final static int KEY_COUNT = 100;
-
     public static void main(String[] args) throws Exception {
-        RedisClient client = RedisClient.create(RedisURI.create("redis://test123@redis-10003.azure1.mague.com:10003/0"));
-        GenericObjectPool<StatefulRedisConnection<String, String>> pool = ConnectionPoolSupport.createGenericObjectPool(() -> client.connect(), new GenericObjectPoolConfig());
+        RedisClient client = RedisClient.create(RedisURI.create("redis://failover-tester@redis-10337.mague.demo-azure.redislabs.com:10337/0"));
+        GenericObjectPool<StatefulRedisConnection<String, String>> pool =
+            ConnectionPoolSupport.createGenericObjectPool(client::connect, new GenericObjectPoolConfig());
         System.out.println("Connection pool to Redis");
         try (StatefulRedisConnection<String, String> connection = pool.borrowObject()) {
-            RedisCommands<String, String> commands = connection.sync();
-            commands.multi();
-            String[] keys = keys();
-            for (String key : keys) {
-                commands.set(key, key + "-value");
-            }
-            commands.exec();
+            RedisAsyncCommands<String, String> commands = connection.async();
+            commands.setAutoFlushCommands(false);
+            RedisFuture<String> future1 = commands.set("Foo", "BAR");
+            RedisFuture<String> future2 = commands.set("Foo2", "BAR2");
+            commands.flushCommands();
+            System.out.println(future1.get());
+            future2.get();
+            
         }
         pool.close();
-        redisClient.shutdown();
-    }
-
-    public static String[] keys() {
-        String[] keys = new String[KEY_COUNT];
-        for (int index = 0; index < KEY_COUNT; index++) {
-            keys[index] = "key" + index;
-        }
-        return keys;
     }
 
 }
